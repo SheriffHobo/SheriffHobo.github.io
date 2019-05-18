c3071b3809eb644c8db0db04a4ce47aa16be6f5b
SheriffHobo <zombiemaster@gmail.com>
2019-05-17 19:01:00 -0700

    added a marquee

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,6 +4,7 @@ $(function () {
     const portfolio = document.getElementById('main');
     const contact = document.getElementById('contact');
 
+    // PORTFOLIO BUTTON
     $('#portfolio').on("click", () => {
         if (portfolio.style.display === 'block') {
             portfolio.style.display = 'none';
@@ -17,7 +18,9 @@ $(function () {
         if (marquee.style.display === 'block') {
             marquee.style.display = 'none';
     };
+    });
 
+    // CONTACT BUTTON
     $('#about').on("click", () => {
         if (contact.style.display === 'block') {
             contact.style.display = 'none';
@@ -31,6 +34,23 @@ $(function () {
         if (marquee.style.display === 'block') {
             marquee.style.display = 'none';
     };
+    });
+
+    // MAIN BUTTON
+    $('#front').on("click", () => {
+        if (contact.style.display === 'block') {
+            contact.style.display = 'none';
+        } else {
+        portfolio.style.display === 'block'
+            portfolio.style.display = 'none';
+        };
+    });
+
+    $('#front').on("click", () => {
+        if (marquee.style.display === 'none') {
+            marquee.style.display = 'block';
+    };
+    });
 
     // MODALS
     $('[data-toggle="popover"]').popover({
